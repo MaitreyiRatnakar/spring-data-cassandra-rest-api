b8277be63784701b125c8cad3d908cbe7774b9d5
MaitreyiRatnakar <maitreyee.ratnakar@gmail.com>
2019-09-09 19:23:06 -0400

    Initial check-in with working service.

---
+++ src/main/java/com/coding_challenge/CodingChallengeApplication.java
@@ -0,0 +1,12 @@
+package com.coding_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingChallengeApplication.class, args);
+	}
+}
---
+++ src/main/java/com/coding_challenge/model/Article.java
@@ -0,0 +1,89 @@
+package com.coding_challenge.model;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.data.cassandra.core.mapping.Column;
+import org.springframework.data.cassandra.core.mapping.PrimaryKey;
+import org.springframework.data.cassandra.core.mapping.Table;
+
+import io.swagger.annotations.ApiModel;
+
+@Table
+@ApiModel
+public class Article {
+
+	@PrimaryKey
+	@NotNull(message = "'id' is a required field.")
+	private Integer id;
+	@Column
+	@NotEmpty(message = "'title' is a required field.")
+	private String title;
+	@Column
+	private String description;
+	@Column
+	private String author;
+	@Column
+	private String tags;
+	@Column
+	private String created_at;
+	@Column
+	private String updated_at;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getTags() {
+		return tags;
+	}
+
+	public void setTags(String tags) {
+		this.tags = tags;
+	}
+
+	public String getCreated_at() {
+		return created_at;
+	}
+
+	public void setCreated_at(String created_at) {
+		this.created_at = created_at;
+	}
+
+	public String getUpdated_at() {
+		return updated_at;
+	}
+
+	public void setUpdated_at(String updated_at) {
+		this.updated_at = updated_at;
+	}
+
+}
